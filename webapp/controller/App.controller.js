936e102626218eee3701aaab5e96d25db493eb5c
kunal-kumar-chaudhary <kkunal644@gmail.com>
2024-07-07 03:41:14 +0530

    controllers

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,5 +1,10 @@
 sap.ui.define(["sap/ui/core/mvc/Controller"] // pulling in our controller assets
     , function (Controller) {
   "use strict";
-  return Controller.extend("", {});
+  return Controller.extend("sap.ui.demo.walkthrough.App", {
+    onShowHello: function () {
+        // show a native alert
+        alert("hello there, button pressed!");
+    }
+  });
 });
