3d1358c5b7f27e6af39e2d24909c153865aab44f
kunal-kumar-chaudhary <kkunal644@gmail.com>
2024-07-19 00:12:58 +0530

    internationalization

--- learning_models/webapp/LandingPage.controller.js
+++ learning_models/webapp/LandingPage.controller.js
@@ -2,15 +2,22 @@ sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageToast",
     "sap/ui/model/json/JSONModel",
-    "sap/ui/model/BindingMode"
-], function(Controller, MessageToast, JSONModel, BindingMode) {
+    "sap/ui/model/BindingMode",
+    "sap/ui/model/resource/ResourceModel"
+], function(Controller, MessageToast, JSONModel, BindingMode, ResourceModel) {
     "use strict";
 
     return Controller.extend("app.LandingPage", {
         // we will initialize the model and can use it throughout the application
         onInit: function() {
+            var i18n = new ResourceModel({
+                bundleName: "app.i18n.i18n"
+            })
+            // set the i18n as a model to the view
+            this.getView().setModel(i18n, "i18n");
+            var oBundle = this.getView().getModel("i18n").getResourceBundle();
             var mData = new JSONModel({
-                "message": "this is a message shown in UI5 demo"
+                "message": oBundle.getText("msg")
             });
             mData.setDefaultBindingMode(BindingMode.OneWay); // for one way binding
             this.getView().setModel(mData);
